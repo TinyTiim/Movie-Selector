20d211395d255725361759d0fe16e3f10f25d53b
Irrumabo <ballenstev@gmail.com>
2023-03-13 20:51:35 -0700

    fixed redirect

--- assets/script.js
+++ assets/script.js
@@ -165,35 +165,33 @@ function getComedy(num) {
     getAction(5);
     getAction(6);
     
-const searchInput = document.getElementById("search");
 
-// Add event listener for key press on search input
+const searchInput = document.getElementById("search");
 searchInput.addEventListener("keyup", function (event) {
-  // Check if enter key is pressed (keyCode 13)
-  if (event.keyCode === 13) {
-    // Call your function here
+   if (event.keyCode === 13) {
+    localStorage.setItem('searchTerm', searchInput.value);
     window.location = "./index2.html";
-  }
-  window.onload = function(){
-    searchMovies();
   }
 });
 
-function searchMovies() {
-  var searchTerm = searchInput.value;
-  //var movieId = searchInput.value;
+window.onload = function(){
+  var searchTerm = localStorage.getItem('searchTerm');
+  if (searchTerm){
+    localStorage.removeItem('searchTerm');
+    searchMovies(searchTerm);
+  }  
+}
+
+function searchMovies(searchTerm) {
   console.log("you have entered " + searchTerm);
 
  var apiUrl = `${movieApiUrl}?apikey=${movieApiKey}&t=${searchTerm}`;
-  //var apiurl = `${movieApiUrl}?apikey=${movieApiKey}&s=$`
   fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
-      // Extract the movie information from the response
       var movieTitle = data.Title;
       var moviePlot = data.Plot;
 
-      // Display the movie information on the page
       var titleElement = document.getElementById("title-src");
       titleElement.textContent = movieTitle;
 
