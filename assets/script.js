b742214e694f9543a98beb82000bc88dd0430f86
Irrumabo <ballenstev@gmail.com>
2023-03-13 19:54:14 -0700

    fixed some of the stuff in the src stuff

--- assets/script.js
+++ assets/script.js
@@ -32,21 +32,15 @@ function getMovie(num) {
     .then(function (data) {
       var movieTitle = data.Title;
       var moviePlot = data.Plot;
-
       var titleElement = document.getElementById("title" + num);
-
       titleElement.textContent = movieTitle;
-
       var plotElement = document.getElementById("plot" + num);
-
       plotElement.textContent = moviePlot;
-      plotElement.setAttribute("id", "plot");
     })
     .catch(function (error) {
       console.error(error);
     });
 }
-
 getMovie(1);
 getMovie(2);
 getMovie(3);
@@ -68,9 +62,7 @@ romanceButton.addEventListener('click', function() {
     var randomMovie = data.Search[num];
     var movieId = randomMovie.imdbID;
 
-  return fetch(
-    `${movieApiUrl}?apikey=${movieApiKey}&s=${romance}&type=${type}&i=${movieId}&plot=short`
-  );
+  return fetch(`${movieApiUrl}?apikey=${movieApiKey}&t=${romance}&i=${movieId}`);
 })
 .then(function (response) {
   return response.json();
@@ -78,25 +70,16 @@ romanceButton.addEventListener('click', function() {
 .then(function (data) {
   var movieTitle = data.Title;
   var moviePlot = data.Plot;
-
   var titleElement = document.getElementById("title" + num);
-
-  titleElement.textContent = movieTitle;
-
+  titleElement.textContent = movieTitle;  
   var plotElement = document.getElementById("plot" + num);
-
-  plotElement.textContent = moviePlot;
-  plotElement.setAttribute("id", "plot");
+  plotElement.textContent = moviePlot;  
 })
 .catch(function (error) {
   console.error(error);
-
-
 })
-
 })
 }
-
 getRomance(1);
 getRomance(2);
 getRomance(3);
@@ -116,34 +99,24 @@ function getComedy(num) {
       // Wrap all below code with for loop 6 times
       console.log(data);
       var randomMovie = data.Search[num];
-      var movieId = randomMovie.imdbID;
+     var movieId = randomMovie.imdbID;
   
-    return fetch(
-      `${movieApiUrl}?apikey=${movieApiKey}&s=${comedy}&type=${type}&i=${movieId}&plot=short`
-    );
+    return fetch(`${movieApiUrl}?apikey=${movieApiKey}&s=${comedy}&type=${type}&i=${movieId}&plot=short`);
   })
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
     var movieTitle = data.Title;
-    var moviePlot = data.Plot;
-  
-    var titleElement = document.getElementById("title" + num);
-  
-    titleElement.textContent = movieTitle;
-  
-    var plotElement = document.getElementById("plot" + num);
-  
+    var moviePlot = data.Plot;  
+    var titleElement = document.getElementById("title" + num);  
+    titleElement.textContent = movieTitle;  
+    var plotElement = document.getElementById("plot" + num);  
     plotElement.textContent = moviePlot;
-    plotElement.setAttribute("id", "plot");
   })
   .catch(function (error) {
     console.error(error);
-  
-  
-  })
-  
+  })  
   })
   }
   
@@ -166,40 +139,66 @@ function getComedy(num) {
         // Wrap all below code with for loop 6 times
         console.log(data);
         var randomMovie = data.Search[num];
-        var movieId = randomMovie.imdbID;
-    
-      return fetch(
-        `${movieApiUrl}?apikey=${movieApiKey}&s=${action}&type=${type}&i=${movieId}&plot=short`
-      );
+        var movieId = randomMovie.imdbID;    
+      return fetch(`${movieApiUrl}?apikey=${movieApiKey}&s=${action}&type=${type}&i=${movieId}&plot=short`);
     })
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       var movieTitle = data.Title;
-      var moviePlot = data.Plot;
-    
-      var titleElement = document.getElementById("title" + num);
-    
+      var moviePlot = data.Plot;    
+      var titleElement = document.getElementById("title" + num);    
       titleElement.textContent = movieTitle;
-    
-      var plotElement = document.getElementById("plot" + num);
-    
+      var plotElement = document.getElementById("plot" + num);    
       plotElement.textContent = moviePlot;
-      plotElement.setAttribute("id", "plot");
     })
     .catch(function (error) {
-      console.error(error);
-    
-    
+      console.error(error);    
+    })    
     })
-    
-    })
-    }
-    
+    }    
     getAction(1);
     getAction(2);
     getAction(3);
     getAction(4);
     getAction(5);
     getAction(6);
+    
+const searchInput = document.getElementById("search");
+
+// Add event listener for key press on search input
+searchInput.addEventListener("keyup", function (event) {
+  // Check if enter key is pressed (keyCode 13)
+  if (event.keyCode === 13) {
+    // Call your function here
+    window.location = "./index2.html";
+  }
+  window.onload = function(){
+    searchMovies();
+  }
+});
+
+function searchMovies() {
+  var searchTerm = searchInput.value;
+  //var movieId = searchInput.value;
+  console.log("you have entered " + searchTerm);
+
+ var apiUrl = `${movieApiUrl}?apikey=${movieApiKey}&t=${searchTerm}`;
+  //var apiurl = `${movieApiUrl}?apikey=${movieApiKey}&s=$`
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      // Extract the movie information from the response
+      var movieTitle = data.Title;
+      var moviePlot = data.Plot;
+
+      // Display the movie information on the page
+      var titleElement = document.getElementById("title-src");
+      titleElement.textContent = movieTitle;
+
+      var plotElement = document.getElementById("plot-src");
+      plotElement.textContent = moviePlot;
+      console.log(moviePlot);
+    })
+}
